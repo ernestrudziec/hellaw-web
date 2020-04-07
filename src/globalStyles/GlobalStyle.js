2065c3662500a71492ce4a9375069872462774bc
ernestrudziec <ernest.rudziec@gmail.com>
2020-04-07 03:29:55 +0200

    0.2.3 deleted import url from Global Style Component (Causing Bug)

--- src/globalStyles/GlobalStyle.js
+++ src/globalStyles/GlobalStyle.js
@@ -5,7 +5,6 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css?family=Baloo+2:400,500,600,700,800|Lato:300,400,700,900|Oxanium:300,400,500,600,700|Roboto+Slab:400,700,800,900&display=swap&subset=latin-ext');
  
 
 body, html{
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RootView from './views/RootView';
-
+import './fonts.css';
 
 
 ReactDOM.render(
